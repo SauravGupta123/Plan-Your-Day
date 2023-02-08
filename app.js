a61188a3a4c90f71d394aec9870a1e48709e48bd
Saurav <er.sauravgpt@gmail.com>
2023-02-08 11:24:18 +0530

    date list and work list is working fine

---
+++ app.js
@@ -0,0 +1,80 @@
+const express= require('express');
+const bodyParser= require('body-parser');
+let ejs = require('ejs');
+const app=express();
+// const fs = require("fs");
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.use( bodyParser.urlencoded({ extended: true }));
+let size=0;
+var items=[];
+let workItems=[];
+
+
+
+let currDate= new Date();
+
+var options = { 
+    weekday: 'long',
+     year: 'numeric', 
+     month: 'long', 
+     day: 'numeric' 
+    };
+
+
+
+currDate= currDate.toLocaleDateString("en-US", options);
+
+app.get('/', (req, res) => {
+  res.render('list', {title: currDate, addTask:items});
+  
+  
+});
+
+//when user adds item and presses Add task button
+app.post('/',(req,res)=>{
+  
+    if(req.body.reqFrom==="Work"){
+      workItems.push(req.body.newTask);
+      res.redirect("/work");
+    }
+    else{
+
+      items.push(req.body.newTask);     //new task is added to the items array
+      
+      
+      res.redirect('/');     // the page is redirected to homepage and new item is rendered automatically
+    }
+
+
+});
+
+
+
+
+//for worklist
+
+app.get("/work",function(req,res){
+
+  res.render('list',{title: "Work list", addTask: workItems });
+})
+
+
+app.post('/work',function(req,res){
+
+});
+
+
+//for about page
+
+app.get("/about",function(req,res){
+
+  res.render('list',{title: "about", addTask: workItems });
+})
+
+
+app.listen(3000,function(){
+console.log("server is started at port 3000");
+
+});
---
+++ index.js
@@ -0,0 +1,6 @@
+let current=document.querySelector("h1").innerHTML;
+
+current=document.querySelector("h1").innerHTML=current+ " sunday";
+current=document.querySelector("h1").innerHTML=current+ " monday";
+
+console.log(document.querySelector("h1").innerHTML);
